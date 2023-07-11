389dc1bb65aa45bee7dcd47c8cccd4734f57bd74
sohostory <sohostory@gmail.com>
2023-07-10 20:57:02 -0500

    added new game button to end game component

--- src/routes/end-game/end-game.component.jsx
+++ src/routes/end-game/end-game.component.jsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from "react-router-dom";
 import StandingList from "../../components/standing-list/standing-list.component";
+import Button from "../../components/button/button.component";
 
 const EndGame = ({ players }) => {
   const navigate = useNavigate();
@@ -10,10 +11,17 @@ const EndGame = ({ players }) => {
     return a.totalScore - b.totalScore;
   });
 
+  const newGame = () => {
+    navigate("/new");
+  };
+
   return (
     <div className="end-game-container">
       <h2>Final Results</h2>
       <StandingList currentStanding={finalStanding} />
+      <Button type="button" onClick={newGame}>
+        Play New Game
+      </Button>
     </div>
   );
 };
