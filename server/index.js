cf1eaed148f9f7c3b1e4325d014a56ffd09e02c0
sohostory <sohostory@gmail.com>
2023-07-12 17:18:28 -0500

    setup fs to read json file from express server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
+const fs = require("fs");
 
 const express = require("express");
 const cors = require("cors");
-
-const { PORT } = process.env;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+fs.readFile("gamedata.json", function (err, data) {
+  if (err) throw err;
+
+  const gamedata = JSON.parse(data);
+  console.log(gamedata);
+});
